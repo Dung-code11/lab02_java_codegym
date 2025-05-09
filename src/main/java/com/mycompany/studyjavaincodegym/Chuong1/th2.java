ffcadc525615cebeeb5ad97dac440102aacf8cae
Dung-code11 <gamsio204@gmail.com>
2025-05-09 20:51:36 +0700

    buoi 1 hoc

---
+++ src/main/java/com/mycompany/studyjavaincodegym/Chuong1/th2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.studyjavaincodegym.Chuong1;
+
+/**
+ *
+ * @author DUNG LE
+ */
+public class th2 {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i = " + i);
+System.out.println("f = " + f);
+System.out.println("d = " + d);
+System.out.println("b = " + b);
+System.out.println("c = " + c);
+System.out.println("s = " + s);
+    }
+}
