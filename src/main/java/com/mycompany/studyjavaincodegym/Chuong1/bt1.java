1114e4e729cfd2866268a2a30d0647b6e960d89c
Dung-code11 <gamsio204@gmail.com>
2025-05-11 03:37:24 +0700

    bt1

---
+++ src/main/java/com/mycompany/studyjavaincodegym/Chuong1/bt1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.studyjavaincodegym.Chuong1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DUNG LE
+ */
+public class bt1 {
+    public static void main(String[] args) {
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("Vui Long Nhap Ten: ");
+        String ten = nhap.nextLine();
+        System.out.printf("Hello: ", ten);
+    }
+}
