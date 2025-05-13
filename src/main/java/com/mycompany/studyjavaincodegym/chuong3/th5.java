565a3d6aac06b0e2f75aa1fbd8b2a9d8d8dbde04
Dung-code11 <gamsio204@gmail.com>
2025-05-13 20:32:13 +0700

    th5

---
+++ src/main/java/com/mycompany/studyjavaincodegym/chuong3/th5.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.studyjavaincodegym.chuong3;
+
+/**
+ *
+ * @author DUNG LE
+ */
+public class th5 {
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
