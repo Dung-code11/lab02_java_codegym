d77c59ed09cac1acb42be8628d1d11eefd3e3969
Dung-code11 <gamsio204@gmail.com>
2025-05-27 16:21:00 +0700

    bai tap chuong 9

---
+++ src/main/java/com/mycompany/studyjavaincodegym/Chuong9/absolutenumbercalculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,12 @@
+package com.mycompany.studyjavaincodegym.Chuong9.absolutenumbercalculator;
+
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        //throw new UnsupportedOperationException();
+        //return 0;
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
